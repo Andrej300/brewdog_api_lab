3c8d48c9ed7414d2c2a85b59ca8d24643e2e249f
Alex Dodd <alexdodd@gmail.com>
2021-01-15 01:08:51 +0000

    few more q's added

--- src/components/FavouriteBeers.vue
+++ src/components/FavouriteBeers.vue
@@ -1,22 +1,29 @@
 <template>
   <div>
     <ul>
-      <li v-for="(beer, index) in favouriteBeers" v-bind:key="index">
-        Name: {{ beer.name }} - ABV: {{ beer.abv }}
-      </li>
+      <li v-for="(beer, index) in favouriteBeers" v-bind:value="beer" v-bind:key="index">
+        Name: {{ beer.name }} - ABV: {{ beer.abv }} 
+        <button v-on:click="removeFromFavourites$beer[index]" v-bind:value="allBrewDogBeers[index]">x</button>  <!-- how do you pass to method?-->
     </ul>
+       </li> 
   </div>
 </template>
 
 <script>
+import { eventBus }  from '../main.js'
 export default {
   name: "favourite-beers",
   computed: {
-    favouriteBeers: function () {
+    favouriteBeers: function () { 
       return this.allBrewDogBeers.filter((beer) => beer.isFavourite);  //<-- this doesn't work?!
     },
   },
   props: ["allBrewDogBeers"],
+  methods: {
+    removeFromFavourites: function() {
+        eventBus.$emit('beer-favourite-remove', this.beer)
+    }
+    }
 };
 </script>
 
